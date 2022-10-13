2466165cd9097dcf28b20070099ca6367e76f2d1
luanfgoncalves <luanfgoncalves@outlook.com>
2022-10-13 17:50:47 -0300

    FEAT: Req 7 concluido, IMPORVEMENT: Melhorias semânticas em Desafio6.sql

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT
-  CAST(MIN(plan_value) AS DECIMAL ( 4 , 2 )) faturamento_minimo,
-  CAST(MAX(plan_value) AS DECIMAL ( 4 , 2 )) faturamento_maximo,
-  CAST(ROUND(AVG(plan_value), 2) AS DECIMAL ( 4 , 2 )) faturamento_medio,
-  CAST(ROUND(SUM(plan_value), 2) AS DECIMAL ( 4 , 2 )) faturamento_total
-FROM SpotifyClone.plans_table p
-INNER JOIN SpotifyClone.users_table u
+  CAST(MIN(plan_value) AS DECIMAL ( 4 , 2 )) AS faturamento_minimo,
+  CAST(MAX(plan_value) AS DECIMAL ( 4 , 2 )) AS faturamento_maximo,
+  CAST(ROUND(AVG(plan_value), 2) AS DECIMAL ( 4 , 2 )) AS faturamento_medio,
+  CAST(ROUND(SUM(plan_value), 2) AS DECIMAL ( 4 , 2 )) AS faturamento_total
+FROM SpotifyClone.plans_table AS p
+INNER JOIN SpotifyClone.users_table AS u
 ON p.plan_id = u.plan_id;
